aa68ac363a3f48b3be94c318a934c153f78fab83
TIMM SNOW <two.tone.snow@gmail.com>
2021-02-24 15:05:14 -0500

    sent a product to my database via seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+all_products = Product.create(name: "rice cooker", price: 385, image_url: "https://supersavings.lk/wp-content/uploads/2019/04/Bright-Rice-Cooker-1.jpg", description: "this is ONE SEXY RICE COOOKER. Impress all your friends with this nasty little hot pot!", rating: "1 star")
