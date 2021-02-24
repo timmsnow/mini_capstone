4040ddeb5b0db0bb1e1ad03f6374b89425a17caf
TIMM SNOW <two.tone.snow@gmail.com>
2021-02-24 15:24:21 -0500

    adds another instance of method Product

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/all_products_path" => "products#all_products_action"
+  end
 end
